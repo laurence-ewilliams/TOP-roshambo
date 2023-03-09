54e493791fb4e4accb57502395973c2ff8790052
laurence-ewilliams <leveritt-williams@outlook.com>
2023-03-09 18:09:57 +0100

    Updated playRound function to handle all outcomes

--- app.js
+++ app.js
@@ -15,10 +15,26 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection === "rock" && computerSelection === "Paper") {
         return "You lose! Paper beats Rock";
     } else if (playerSelection === "rock" && computerSelection === "Scissors") {
-        return "You win! Rock beats Scissors"
+        return "You win! Rock beats Scissors";
+    }
+
+    if (playerSelection === "paper" && computerSelection === "Paper") {
+        return "It's a tie, go again!";
+    } else if (playerSelection === "paper" && computerSelection === "Scissors") {
+        return "You lose! Scissors beats Paper";
+    } else if (playerSelection === "paper" && computerSelection === "Rock") {
+        return "You win! Paper beats Rock";
+    }
+
+    if (playerSelection === "scissors" && computerSelection === "Scissors") {
+        return "It's a tie, go again!";
+    } else if (playerSelection === "scissors" && computerSelection === "Rock") {
+        return "You lose! Rock beats Scissors";
+    } else if (playerSelection === "scissors" && computerSelection === "Paper") {
+        return "You win! Scissors beats Paper";
     }
 }
 
 const computerSelection = getComputerChoice();
 const playerSelection = prompt("Select your weapon: Rock, Paper or Scissors").toLowerCase();
-console.log(playRound("rock", computerSelection));
+console.log(playRound(playerSelection, computerSelection));
